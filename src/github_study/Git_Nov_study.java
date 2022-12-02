d26944a1d48b8d58fdf2cd074e2831eddf813b42
hp <hp@DESKTOP-R4KN3VC>
2022-12-02 16:26:39 +0530

    Git 1st code

---
+++ src/github_study/Git_Nov_study.java
@@ -0,0 +1,10 @@
+package github_study;
+
+public class Git_Nov_study {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("1st code github");
+	}
+
+}
