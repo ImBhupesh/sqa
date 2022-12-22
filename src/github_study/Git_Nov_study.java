42bc6b34c854e3c78044c26f17738ab2d2024183
hp <hp@DESKTOP-R4KN3VC>
2022-12-22 21:35:16 +0530

    New Code Added

--- src/github_study/Git_Nov_study.java
+++ src/github_study/Git_Nov_study.java
@@ -5,6 +5,8 @@ public class Git_Nov_study {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("1st code github");
+System.out.println("2st code github");
+
 	}
 
 }
